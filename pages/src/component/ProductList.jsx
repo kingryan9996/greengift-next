f67b91d3cd6dadad4dfc226d34a40c357f78eb90
kingryan9996 <kingryan9996@gmail.com>
2023-03-28 05:18:07 +0900

    server

--- pages/src/component/ProductList.jsx
+++ pages/src/component/ProductList.jsx
@@ -53,7 +53,7 @@ const ProductList = ({visible,setVisible}) => {
     const GetApi = (obj)=>{
         keyWo.current = obj;
         
-        axios.get(`https://port-0-green-gift-back-6g2llfc6npfv.sel3.cloudtype.app/search/shop?query=${obj}&display=${numRef.current}`)
+        axios.get(`https://port-0-node-greengift-nx562olfqkxd9g.sel3.cloudtype.app/search/shop?query=${obj}&display=${numRef.current}`)
         .then((res) => {
             setThenApi(res.data.items)
           });
@@ -61,7 +61,7 @@ const ProductList = ({visible,setVisible}) => {
   
         const dragApi = (obj) => {
   
-          axios.get(`https://port-0-green-gift-back-6g2llfc6npfv.sel3.cloudtype.app/search/shop?query=${obj}&display=${numRef.current}`)
+          axios.get(`https://port-0-node-greengift-nx562olfqkxd9g.sel3.cloudtype.app/search/shop?query=${obj}&display=${numRef.current}`)
           .then((res) => {
             setThenApi(res.data.items)
             numRef.current+=10
